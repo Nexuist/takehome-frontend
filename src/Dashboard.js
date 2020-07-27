36f5dfc5e9c5ea5fa5715f3e3fefa13c5d6212d1
Andi Andreas <nexuister@gmail.com>
2020-07-27 19:18:20 -0400

    Completed dashboard skeleton

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -12,6 +12,7 @@ import {
 } from "react-bootstrap";
 import "./dashboard.css";
 import { PlusCircle, ShoppingCart, Edit, Star } from "react-feather";
+import { Line } from "react-chartjs-2";
 
 function ProductItem(props) {
   return (
@@ -22,6 +23,33 @@ function ProductItem(props) {
     </Nav.Item>
   );
 }
+
+const sampleData = {
+  labels: ["January", "February", "March", "April", "May", "June", "July"],
+  datasets: [
+    {
+      label: "My First dataset",
+      fill: false,
+      lineTension: 0.1,
+      backgroundColor: "rgba(75,192,192,0.4)",
+      borderColor: "rgba(75,192,192,1)",
+      borderCapStyle: "butt",
+      borderDash: [],
+      borderDashOffset: 0.0,
+      borderJoinStyle: "miter",
+      pointBorderColor: "rgba(75,192,192,1)",
+      pointBackgroundColor: "#fff",
+      pointBorderWidth: 1,
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: "rgba(75,192,192,1)",
+      pointHoverBorderColor: "rgba(220,220,220,1)",
+      pointHoverBorderWidth: 2,
+      pointRadius: 1,
+      pointHitRadius: 10,
+      data: [65, 59, 80, 81, 56, 55, 40],
+    },
+  ],
+};
 
 function Dashboard(props) {
   return (
@@ -104,10 +132,7 @@ function Dashboard(props) {
                 <Card>
                   <Card.Header as="h5">Sales</Card.Header>
                   <Card.Body>
-                    <Card.Text>
-                      With supporting text below as a natural lead-in to
-                      additional content.
-                    </Card.Text>
+                    <Line data={sampleData} />
                   </Card.Body>
                 </Card>
               </Col>
@@ -118,9 +143,7 @@ function Dashboard(props) {
                     <Edit size={18} color="blue" className="float-right" />
                   </Card.Header>
                   <Card.Body>
-                    <Card.Text>
-                      <h1>200</h1>
-                    </Card.Text>
+                    <h1>200</h1>
                   </Card.Body>
                 </Card>
               </Col>
